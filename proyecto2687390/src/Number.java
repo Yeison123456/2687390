7aa00a63a28b74f578211186c587921f313abe2a
YDRR123 <yeisonrodrigueznacun@gmail.com>
2023-02-14 09:42:04 -0500

    se agrega la clase Number

---
+++ proyecto2687390/src/Number.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Number {
+    public static void main(){
+        int numberOne, numberTwo, suma, resta;
+
+        Scanner lectura = new Scanner (System.in);
+        System.out.println("Ingresa el primer numero");
+        numberOne = lectura.nextInt();
+
+        System.out.println("Ingresa el segundo numero");
+        numberTwo = lectura.nextInt();
+
+ 
+
+
+        if( numberOne>numberTwo ){
+            suma= numberOne + numberTwo;
+            System.out.println("El numero mayos es " + numberOne + "y el numero nemor es " + numberTwo +",la resta entre estos dos numero es " + suma);
+        }
+        else{
+            resta= numberOne - numberTwo;
+            System.out.println("El numero mayos es " + numberTwo + "y el numero nemor es " + numberOne +",la resta entre estos dos numero es " + resta);
+        }
+
+        lectura.close();
+    }
+}
