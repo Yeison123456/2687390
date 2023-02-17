43f701be3baece2b705cf2a1ac33e6f5dc90ad6d
Yeison123456 <yeisonrodrigueznacun@gmail.com>
2023-02-17 13:51:52 -0500

    se agrego Random

--- proyecto2687390/src/Random.java
+++ proyecto2687390/src/Random.java
@@ -8,20 +8,20 @@ public static void main(String[] args) {
         String eleccion;
 
         Scanner lectura = new Scanner (System.in);
-        System.out.println("Cara o cruz");
+        System.out.println("Cara o sello");
         eleccion = lectura.nextLine();
 
         if( eleccion.equals("cara") && numero==1 ){
             System.out.println("Usted escogio cara y gano :D");
         }
-        else if( eleccion.equals("cruz") && numero==2 ){
-            System.out.println("Usted escogio cruz y gano :,(");
+        else if( eleccion.equals("sello") && numero==2 ){
+            System.out.println("Usted escogio sello y gano :D");
         }
         else if( eleccion.equals("cara") && numero==2){
-            System.out.println("Usted escogio cara y perdiste");
+            System.out.println("Usted escogio cara y perdiste :,(");
         }
-        else if( eleccion.equals("cruz") && numero==1){
-            System.out.println("Ested escogio cruz y perdio");
+        else if( eleccion.equals("sello") && numero==1){
+            System.out.println("Ested escogio sello y perdio :,(");
         }
 
         lectura.close();
