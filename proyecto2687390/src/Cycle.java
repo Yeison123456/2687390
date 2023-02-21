4d4c5cd71d8bb54f5a1c9813a61b577bac1f0ea6
Yeison123456 <yeisonrodrigueznacun@gmail.com>
2023-02-21 11:58:54 -0500

    Se sgrego Cycle y CyclePractice

---
+++ proyecto2687390/src/Cycle.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Cycle {
+    public static void main(String[] args) {
+        int precio, cantidad, contador, subTotal, total, billete, cambio;
+        Scanner lectura= new Scanner(System.in);
+
+            contador=0;
+            total=0;//acumulador//
+            for( int x=1; x<=5; x++){
+                System.out.println("Digite el precio del producto");
+                precio=lectura.nextInt();
+
+                System.out.println("Digite la cantidad del producto");
+                cantidad=lectura.nextInt();
+
+                contador=contador+1;
+                subTotal=precio*cantidad;
+                total=total+subTotal;
+                System.out.println("El subtotal es de " + subTotal);
+            }
+            System.out.println("El valor a pagar es de " + total);
+            System.out.println("¿Con cuanto vas a pagar?");
+            billete=lectura.nextInt();
+            cambio=billete-total;
+            System.out.println("Su cambio es de " + cambio);
+
+            lectura.nextLine();
+            String respuesta;
+            System.out.println("¿Cuenta con telefonia movil exito?");
+            respuesta=lectura.nextLine();
+
+            if(respuesta.equals("si")){
+                System.out.println("Los minutos que obtuvo con su compra son " + contador);
+            }
+            else {
+                System.out.println("No pierdas mas minutos, adquiere ya tu telefonia movil exito");
+            }
+
+            lectura.close();
+    }
+}
---
+++ proyecto2687390/src/CyclePractice.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ public class CyclePractice { public static void main(String[] args) { 
+    Scanner lectura = new Scanner(System.in); 
+    int contadorH, contadorM; 
+    String genero; 
+    contadorH = 0; contadorM = 0;
+    for(int x=1; x<=10; x++){ 
+        System.out.println(" Digita si eres hombre o mujer "); 
+        genero = lectura.nextLine(); 
+        if(genero.equalsIgnoreCase("Hombre")){ 
+            contadorH = contadorH+1; }
+        else if(genero.equalsIgnoreCase("Mujer")){
+         contadorM = contadorM+1; } } 
+         System.out.println(" Total de hombres en el grupo es " + contadorH); 
+         System.out.println(" Total de mujeres en el grupo es " + contadorM); 
+         lectura.close();
+         } }
